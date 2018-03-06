a6c202e824e36822a3a44fd6b89e3199269651c1
Morix <rade1500@gmail.com>
2018-03-06 12:59:04 +0000

    complate 3

--- doller.go
+++ doller.go
@@ -4,8 +4,13 @@ type Doller struct{
     Amount int
 }
 
-func (d *Doller) times(cnt int) Doller {
+func (d Doller) times(cnt int) Doller {
 	return Doller{
         Amount: d.Amount * cnt,
     }
+}
+
+func (d Doller) equals(object interface{}) bool {
+    o := object.(Doller)
+    return d.Amount == o.Amount
 }
--- doller_test.go
+++ doller_test.go
@@ -6,14 +6,23 @@ import (
 
 func TestMultipliction(t *testing.T) {
 	five := Doller{Amount: 5}
-	product := five.times(2)
-	if got, want := product.Amount, 10; got != want {
+	if got, want := five.times(2).Amount, 10; got != want {
 		t.Errorf("amount: got %v want %v", got, want)
 	}
 
-    product = five.times(3)
-	if got, want := product.Amount, 15; got != want {
+	if got, want := five.times(3).Amount, 15; got != want {
 		t.Errorf("amount: got %v want %v", got, want)
 	}
+}
 
+func TestEquality(t *testing.T)  {
+    a := Doller{Amount: 5}
+    b := Doller{Amount: 5}
+    if !a.equals(b) {
+        t.Error("equals: got false want true")
+    }
+    c := Doller{Amount: 6}
+    if a.equals(c) {
+        t.Error("equals: got true want false")
+    }
 }
