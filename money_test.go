9007271e935c871ff1dbe0ad31ce7c15006ef38d
Morix <rade1500@gmail.com>
2018-03-08 03:25:37 +0000

    remove verbose

--- money_test.go
+++ money_test.go
@@ -14,15 +14,7 @@ func TestMultipliction(t *testing.T) {
 func TestEquality(t *testing.T) {
 	assert.True(t, NewDollar(5).equals(NewDollar(5)), "amount is not equals")
 	assert.False(t, NewDollar(6).equals(NewDollar(5)), "amount is equals")
-	assert.True(t, NewFranc(5).equals(NewFranc(5)), "amount is not equals")
-	assert.False(t, NewFranc(6).equals(NewFranc(5)), "amount is equals")
 	assert.False(t, NewDollar(5).equals(NewFranc(5)), "amount is equals")
-}
-
-func TestFrancMultipliction(t *testing.T) {
-	five := NewFranc(5)
-	assert.Equal(t, five.times(2), NewFranc(10), "times function have problem")
-	assert.Equal(t, five.times(3), NewFranc(15), "times function have problem")
 }
 
 func TestCurrency(t *testing.T) {
