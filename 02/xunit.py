614e9018d8747c156812ef72cad49ad51fadc1cd
Morix <rade1500@gmail.com>
2018-03-08 10:37:31 +0000

    complate 21

--- 02/xunit.py
+++ 02/xunit.py
@@ -1,3 +1,11 @@
+class TestResult:
+    def __init__(self):
+        self.runCount = 0 
+    def testStarted(self):
+        self.runCount += 1
+    def summary(self):
+        return "%d run, 0 failed" % self.runCount
+
 class TestCase:
     def __init__(self, name):
         self.name = name
@@ -6,10 +14,13 @@ class TestCase:
     def tearDown(self):
         pass
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return result
 
 class WasRun(TestCase):
     def setUp(self):
@@ -18,11 +29,24 @@ class WasRun(TestCase):
         self.log += "testMethod "
     def tearDown(self):
         self.log += "tearDown "
+    def testBrokenMethod(self):
+        raise Exception
 
 class TestCaseTest(TestCase):
     def testSetUp(self):
         test = WasRun("testMethod")
         test.run()
         assert("setUp testMethod tearDown " == test.log)
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
+        
 
 TestCaseTest("testSetUp").run()
+TestCaseTest("testResult").run()
+#TestCaseTest("testFailedResult").run()
