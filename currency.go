645a8089e617b1e5819a8e445e5cdb5f3d3823b5
Morix <rade1500@gmail.com>
2018-03-08 03:00:46 +0000

    add currency

--- currency.go
+++ currency.go
@@ -2,12 +2,14 @@ package money
 
 func NewDollar(amount int) *Money {
 	return &Money{
-		amount: amount,
+		amount:   amount,
+		currency: "USD",
 	}
 }
 
 func NewFranc(amount int) *Money {
 	return &Money{
-		amount: amount,
+		amount:   amount,
+		currency: "CHF",
 	}
 }
--- money.go
+++ money.go
@@ -1,16 +1,18 @@
 package money
 
 type Money struct {
-	amount int
+	amount   int
+	currency string
 }
 
 func (m Money) equals(object interface{}) bool {
 	o := object.(*Money)
-	return m.amount == o.amount
+	return m.amount == o.amount && m.currency == o.currency
 }
 
 func (m Money) times(cnt int) *Money {
 	return &Money{
-		amount: m.amount * cnt,
+		amount:   m.amount * cnt,
+		currency: m.currency,
 	}
 }
